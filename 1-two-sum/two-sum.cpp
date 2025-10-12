09953de5c9930bdd353abed391bed48213cbe09b
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-10-12 14:18:03 +0530

    Time: 4 ms (55.95%) | Memory: 14.9 MB (36.09%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,20 +1,12 @@
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
-        vector<int> ans;
-        
-            for(int i=0; i<nums.size(); i++){
-                for(int j=0; j<nums.size(); j++){
-                    if(i!=j){
-                    if(nums[i]+nums[j]==target){
-                        ans.push_back(i);
-                        ans.push_back(j);
-                        return {i,j};
-                    }                
-                    } 
-                }
-            }
-    return {};}
+    unordered_map<int,int> mpp;
+    int n= nums.size();
+    for(int i=0; i<=n; i++){
+        int compliment= target - nums[i];
+        if (mpp.count(compliment)) return {mpp[compliment],i};
+        else mpp.emplace(nums[i],i);
+    }
+    return{};}
 };
