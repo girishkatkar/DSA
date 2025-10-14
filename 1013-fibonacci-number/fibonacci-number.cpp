e3232b3f61ed2f6ba6630531954fc47b372b00d7
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-10-15 02:29:20 +0530

    Time: 2 ms (77.83%) | Memory: 7.6 MB (86.83%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,3 +1,14 @@
+//WTF WHO THOUGHT OF THIS?
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if (n <= 1)
+//             return n;
+
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// };
+
 class Solution {
 public:
 
