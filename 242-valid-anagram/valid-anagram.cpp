81136a353911c019c6f2c0dcd3b9907852b8f97d
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-10-21 00:12:57 +0530

    Time: 3 ms (51.31%) | Memory: 9.6 MB (74.19%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        vector<int> hashs(26,0);
-        vector<int> hasht(26,0);
-        for(int i=0; i<s.size(); i++){
-            hashs[s[i]-'a']++;
-        }
-        for(int i=0; i<s.size(); i++){
-            hasht[t[i]-'a']++;
-        }
-    return hashs==hasht;}
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+    return s==t;}
 };
