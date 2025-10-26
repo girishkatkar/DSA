82962a4a615f2134677c02aee93f5399693118c0
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-10-26 23:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (82.16%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -6,6 +6,10 @@ public:
             if (nums[i]!=0) {
                 swap(nums[i],nums[left]);
                 left++;
+                //until a 0 is found in array, every iteration of i
+                //will swap with itself as left and i are moving
+                //together, until a 0 comes and left will be stuck
+                //at that position
             }
         }
 
