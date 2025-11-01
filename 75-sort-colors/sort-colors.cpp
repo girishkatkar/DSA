314a4b5ae1ea0adb565547be0820c4da6b22ad28
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-11-01 19:17:28 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.95%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //only three colours 0 1 and 2 
-        //set a counter for all three and insert that amount into 
-        //original array
-        int count0=0;
-        int count1=0;
-        int count2=0;
-        for(int i=0; i<nums.size(); i++){
-            if (nums[i]==0) count0++;
-            else if (nums[i]==1) count1++;
-            else count2++;
-        }
-        nums.clear();
-        for(int i=0; i<count0; i++){
-            nums.push_back(0);
-        }
-        for(int i=0; i<count1; i++){
-            nums.push_back(1);
-        }
-        for(int i=0; i<count2; i++){
-            nums.push_back(2);
+        //Dutch National Flag Algorithm
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        while(mid<=high){
+            if (nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++; mid++;
+            }
+            else if (nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
         }
     }
 };
