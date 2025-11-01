b4e4a3710a88890ddc5b7cdc2caaed0a7014257a
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-11-01 19:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (78.84%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -6,13 +6,26 @@ public:
         int mid=0;
         int high=nums.size()-1;
         while(mid<=high){
+
+            // if moving pointer encounters 0 we cannot move over it 
+            // even though it is a lesser value since it will mix with
+            // 1s which are already behind mid, so we'll throw the 0s
+            // to another pointer 'low' which keeps distinction between
+            // 0s and 1s, and we can swap between 'low' and 'mid', 
+            // low will throw a 1.
             if (nums[mid]==0){
                 swap(nums[low],nums[mid]);
                 low++; mid++;
             }
+
+            //if moving pointer encounters 1, we can move over it
+            // as we'll be keeping 1s before mid point
             else if (nums[mid]==1){
                 mid++;
             }
+
+            //if moving pointer encounters 2 throw it towards
+            //extreme right and reduce array size
             else{
                 swap(nums[mid],nums[high]);
                 high--;
@@ -20,3 +33,9 @@ public:
         }
     }
 };
+
+// BRUTE FORCE: Merge sort
+// BETTER : keeping a small hash map or three counters for 0,1 & 2
+//          clear the vector using vec.clear() and push the elements
+//          accordingly
+// OPTIMAL : Dutch National Flag Algorithm
