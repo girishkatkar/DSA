b9d3e431132323cc52359750fe9196abf68e0586
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-11-02 03:28:59 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (28.85%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        return nums[n/2];
+//Moore's Voting Algorithm
+// moore's voting algo uses concept of pair wise elimination. If the question already states that majority element is more than n/2 i.e. it will be covering more than half the array, that means even if we cancel, majority element with minority one or minority element with majority one, the remaining element is 100% going to be majority one. If on any condition the last element with positive count is not majority one that means  there is no majority element in the array since if it was majority it was never supposed to be cancelled with minority.
+
+    int element;
+    int count=0;
+    for(int i=0; i<nums.size(); i++){
+        if (count==0) {element=nums[i]; count=1;}
+        else if (element==nums[i]) count++;
+        else count--;
     }
+    return element;}
 };
+
+//Time Complexity: O(n)
+//Space Complexity; O(1)
+
+// BRUTE: for loops
+// BETTER: hashing (nlogn or 2n), inbuilt sort(nlogn)
+// OPTIMAL: Moore's voting algo
