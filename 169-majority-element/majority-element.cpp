5c6e05266e15262a9989a1c1fef796a1197443cd
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-11-02 01:05:42 +0530

    Time: 4 ms (20.28%) | Memory: 28.1 MB (87.14%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mpp;
-        for(int i=0; i<nums.size(); i++){
-            mpp[nums[i]]++;
-        }
-        //frequency map created
-        //iterate through the mpp
-        int maxfreq=0;
-        int maxelement;
-        for(auto it: mpp){
-            if (it.second>maxfreq) {maxfreq=it.second;
-            maxelement=it.first;}
-        }
-    return maxelement;}
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        return nums[n/2];
+    }
 };
