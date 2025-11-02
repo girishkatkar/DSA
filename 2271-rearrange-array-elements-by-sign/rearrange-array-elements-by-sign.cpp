a9468bd186973741c03ddb09db7171f9b9103346
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-11-02 19:18:04 +0530

    Time: 12 ms (26.01%) | Memory: 138.9 MB (27.02%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        //Brute force approach
         vector<int> positive;
         vector<int> negative;
-        for(int i=0; i<nums.size(); i++){
+        for(int i=0; i<nums.size(); i++){ //O(n)
             if (nums[i]<0) negative.push_back(nums[i]);
             else positive.push_back(nums[i]);
         }
         int k=0;
-        for(int i=0; i<nums.size(); i=i+2){
+        for(int i=0; i<nums.size(); i=i+2){ //O(n/2)
             nums[i]=positive[k]; k++;
         }
         int j=0;
-        for(int i=1; i<nums.size(); i=i+2){
+        for(int i=1; i<nums.size(); i=i+2){ //O(n/2)
             nums[i]=negative[j]; j++;
         }
     return nums;}
 };
+
+//Time complexity: O(n)+O(n/2)+O(n/2) = O(2n)
+//Space Complexity: O(n)
