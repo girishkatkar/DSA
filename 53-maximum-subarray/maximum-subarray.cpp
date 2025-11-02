2b325b8c4398708ef04514f19ef1fa69fdad367a
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-11-02 14:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (17.33%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        //Kadane's Algorithm
+        int sum=0;
+        int maxsum=INT_MIN;
+        for(int i=0; i<nums.size(); i++){
+            sum=sum+nums[i];
+            maxsum=max(maxsum,sum);
+            if (sum<0) sum=0;
+        }
+    return maxsum;}
+};
