b629ab5b993eafeea095123fd90ce3964dd73d12
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-11-06 03:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (79.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        //using hasharrays to mark the rows and columns
+        // row size = matrix[0].size()
+        // column size = matrix.size()
+        // first row = matrix[0][j]
+        // first column = matrix[0][i
         
-        vector<int> row(matrix.size(),0);
-        vector<int> column(matrix[0].size(),0);
-
+        //first iterate in array and mark the first row and column
+        //who are being used as substitute for hash arrays
+        int col0=1;
         for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                if (matrix[i][j]==0) {
-                    row[i]=1;
-                    column[j]=1;
+            for(int j=0; j<matrix[0].size();j++){
+                if (matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if (j!=0)
+                        {matrix[0][j]=0;}
+                    else col0=0;
                 }
             }
         }
 
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                if (row[i]==1||column[j]==1) {
-
-                    matrix[i][j]=0;
-                }
+        for(int i=1; i<matrix.size(); i++){
+            for(int j=1; j<matrix[0].size();j++){
+                if (matrix[0][j]==0||matrix[i][0]==0) 
+                    {matrix[i][j]=0;}
             }
         }
 
+        if (matrix[0][0]==0) {
+            for(int j=0; j<matrix[0].size(); j++){
+                matrix[0][j]=0;
+            }
+        }
+
+        if (col0==0) {
+            for(int i=0; i<matrix.size(); i++){
+                matrix[i][0]=0;
+            }
+        }
         
 
-    }
+        
+
+
+
+
+
+
+
+}
 };
