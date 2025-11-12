3590c6d043e1fcc0bfadb4f3b8babc0b068ea644
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-11-13 01:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (88.71%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,28 +1,20 @@
 class Solution {
+private:
+    vector<int> pascal(int row){
+        vector<int> temp;
+        temp.push_back(1);
+        int res=1;
+        for(int i=1; i<row; i++){
+            res=res*(row-i);
+            res=res/i;
+            temp.push_back(res);
+        }
+    return temp;}
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        int n=numRows;
-
-        for(int i=1; i <= n; i++){
-            vector<int> temp;
-            temp.push_back(1);
-            if (i==1) {
-                ans.push_back(temp);
-            }
-            else if (i==2) {
-                temp.push_back(1);
-                ans.push_back(temp);
-            }
-            else {
-                
-                for(int i=0; i<ans[ans.size()-1].size()-1; i++){                temp.push_back(ans[ans.size()-1][i]+ans[ans.size()-1][i+1]);
-            
-                    
-                }
-                temp.push_back(1);
-                ans.push_back(temp);
-            }
+        for(int i=1; i<=numRows; i++){
+            ans.push_back(pascal(i));
         }
     return ans;}
 };
