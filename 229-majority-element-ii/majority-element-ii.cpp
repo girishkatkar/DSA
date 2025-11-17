27ff0e18a3d271713aa1f6d8fb89004425f0a0a4
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-11-18 03:26:00 +0530

    Time: 9 ms (26.82%) | Memory: 26.2 MB (43.84%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-       unordered_map<int,int> mpp;
-       //number as key and count as value
-       for(int i=0; i<nums.size(); i++){
-        mpp[nums[i]]++;
-       } 
-       for(auto it: mpp){
-        if (it.second>(nums.size()/3)) ans.push_back(it.first);
-       }
-    return ans;}
+        //using hash_map to update frequency and checking if
+        //frequency exceeded n/3 in the same loop.
+        int n=nums.size();
+        int j=0;
+        vector<int> ret;
+        unordered_map<int,int> mpp;
+        unordered_map<int,int> ans;
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]]++;
+            if (mpp[nums[i]]>n/3){
+                if (ans.count(nums[i])) {}
+                else {ans.emplace(nums[i],0);
+                      ret.push_back(nums[i]);}
+            }
+        }
+    return ret;}
 };
+//TC = O(n)
+//SC = O(n)
