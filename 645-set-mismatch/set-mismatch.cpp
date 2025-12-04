7f6267da7152a8824829c4e1c4d6253e7c8b289a
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-12-05 01:17:08 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (97.71%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        //better solution
-        //using hashmap
-        int n=nums.size();
-        vector<int> hashmap(n+1,0);
+        //optimal solution
+        //using math
+        //we will use the difference between the actual array and expected array
+        //to get the answer. 
+        // The difference between actual and expected will give us 1 equation
+        //The differene between (sum of squares of array elements) of actual and 
+        //expected will give us another equation
+
+        //reminder: use long long and typecasting
+        //sum actual array SN, square actual array SN2
+        //sum expected array SNn, square expected array SN2n
+
+        //SN-SNn = x-y             SN2 - SN2n = x^2 - y^2 
+        //where x is repeating element and y is missing element.
+
+        long long SN=0, SN2=0;
+        //using formulas to find expected values
+        long long n= nums.size();
+        long long SNn= (n*(n+1))/2;
+        long long SN2n= (n*(n+1)*(2*n+1))/6;
+        //using for loop to find expected values
         for(int i=0; i<n; i++){
-            hashmap[nums[i]]++;
+            SN+=nums[i];
+            SN2+=nums[i]*nums[i];
         }
 
-        int repeating;
-        int missing;
-        for(int i=1; i<=n; i++){
-            if (hashmap[i]==2) repeating=i;
-            if (hashmap[i]==0) missing=i;
-        }
-    return {repeating,missing};}
+        //SN - SNn = x - y;
+        long long val1= SN - SNn; // x - y
+        long long val2= SN2 - SN2n; //val2 = x^2 - y^2 = (x+y)(x-y)
+        val2=val2/val1; //val2 is currently storing x+y 
+        //since we have val1= x-y and val2= x+y
+        //add both
+        //val1+val2=  2x
+        long long x= (val1+val2) / 2;
+        //x-y= val1, x-val1= y
+        long long y= x - val1;
+    //type cast it to int since we want to return an integer value
+    return {(int)x, (int)y};}
 };
