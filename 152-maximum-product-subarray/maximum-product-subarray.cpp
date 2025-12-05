5e444b6a2c061c0e46902162f98e9dd86ceee688
Girish Katkar <92095696+girishkatkar@users.noreply.github.com>
2025-12-06 00:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.63%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -6,11 +6,11 @@ public:
         int suffix=1;
         int n=nums.size();
         for(int i=0; i<n; i++){
+            if (prefix==0) prefix=1;
+            if (suffix==0) suffix=1;
             prefix=prefix*nums[i];
             suffix=suffix*nums[n-i-1];
             maxproduct=max(maxproduct, max(prefix,suffix));
-            if (prefix==0) prefix=1;
-            if (suffix==0) suffix=1;
         }
     return maxproduct;}
 };
